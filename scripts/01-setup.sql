1831f76c089af014425929a33c64b4a143ee625b
Joshua-K1234 <horse-january@outlook.com>
2025-05-11 19:28:31 +0100

    Update 01-setup.sql

--- scripts/01-setup.sql
+++ scripts/01-setup.sql
@@ -1,7 +1,18 @@
--- Title: Create Database and Schemas
--- Overview: This script checks if the 'DataWarehouse' database exists and creates it if not. 
--- It then switches to the 'DataWarehouse' database and creates three schemas: 'bronze', 'silver', and 'gold' 
--- only if they don't already exist.
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
 
 USE master;
 GO
