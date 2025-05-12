059758558db36ae31235dc3c21fbb0c46332b189
Joshua-K1234 <horse-january@outlook.com>
2025-05-12 15:30:48 +0100

    Update 05-Inserts.sql

--- scripts/05-Inserts.sql
+++ scripts/05-Inserts.sql
@@ -1,5 +1,26 @@
 USE DataWarehouse
 
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC Silver.load_silver;
+===============================================================================
+*/
+	
+
 -- Truncate all tables in the silver schema before inserting new data
 TRUNCATE TABLE silver.crm_cust_info;
 TRUNCATE TABLE silver.crm_prd_info;
